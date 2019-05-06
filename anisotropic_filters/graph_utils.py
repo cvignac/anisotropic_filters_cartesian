5145556280597a34bb759cd2ea5f39a6c8cb55d9
Clement Vignac <clement.vignac@epfl.ch>
2019-05-06 21:04:33 +0200

    Fixed directed graphs

--- anisotropic_filters/graph_utils.py
+++ anisotropic_filters/graph_utils.py
@@ -10,8 +10,8 @@ class ExtendedGraph(graphs.Graph):
             k (int): maximum power of the polynomials. """
         self.k, self._k = k, k + 1      # Polynomial of degree k is included
         A_full = A.toarray() if sp.sparse.issparse(A) else A
-        directed = not np.allclose(A_full, A_full.T)
-        if directed:
+        self.directed = not np.allclose(A_full, A_full.T)
+        if self.directed:
             super().__init__(A, lap_type='combinatorial')
             self.L = self.L.toarray()
         else:
@@ -25,12 +25,25 @@ class ExtendedGraph(graphs.Graph):
 
     def compute_chebychev_polynomials(self):
         """ Compute the Chebychev polynomials up to degree k (included). """
-        cheb = torch.empty((self._k, self.N, self.N), requires_grad=False, device="cpu")
+        cheb = torch.empty((self._k, self.N, self.N), requires_grad=False,
+                           dtype=torch.float)
         cheb[0] = torch.eye(self.N)
         if self._k > 1:
             cheb[1] = self.L
         for i in range(2, self._k):
             cheb[i] = 2 * torch.matmul(self.L, cheb[i - 1]) - cheb[i - 2]
+        if not self.directed:
+            return cheb
+
+        L = self.L.transpose(0, 1)
+        cheb_inv = torch.empty((self._k, self.N, self.N), requires_grad=False,
+                               dtype=torch.float)
+        cheb_inv[0] = torch.eye(self.N)
+        if self._k > 1:
+            cheb_inv[1] = L
+        for i in range(2, self._k):
+            cheb_inv[i] = 2 * torch.matmul(L, cheb_inv[i - 1]) - cheb_inv[i - 2]
+        cheb = torch.cat([cheb, cheb_inv[1:]])
         return cheb
 
 
